5f4a46d8ce8490fd92cb1b0e8937d307b678891e
kimi <keddy406@hotmail.com>
2020-08-28 00:05:45 +0800

    upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby hell of health goth vexillologist, yr echo park fixie tumblr
+        letterpress man braid. Farm-to-table disrupt cliche master cleanse,
+        selvage beard distillery. Organic tbh art party locavore. Try-hard banjo
+        four dollar toast pok pok roof party humblebrag cray pour-over swag
+        fingerstache tbh. Air plant af marfa occupy. Direct trade hammock 8-bit
+        viral cray waistcoat coloring book DIY marfa cronut.
+      </p>
+    </section>
+  );
+}
